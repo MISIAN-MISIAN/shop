060ab753ebc568e1b2eb505762be0326a72a1c55
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-03-01 19:43:24 +0900

    postsテーブル作成
    postsテーブル作成

---
+++ database/migrations/2020_03_01_181817_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',191);
+            $table->string('brand',191);
+            $table->string('image',191)->nullable();
+            $table->integer('user_id'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
