c20c5f7f013fc9720edc2e6583559665b64d1649
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-03-02 23:39:31 +0900

    投稿
    投稿機能の記載追加

--- resources/views/posts/new.blade.php
+++ resources/views/posts/new.blade.php
@@ -14,39 +14,53 @@
     <section class="main__block">
       <h2 class="sell__block__head">スニーカの写真を入力してください</h2>
       <div class="sell__block__form">
-        <div class="sell__block__form__upload">
-          <h3 class="sell__block__form__upload__head">
-            スニーカの写真
-            <span class="require">必須</span>
-          </h3>
-          <div id="image-box-1">
-            <div class="item-num-0" id="image-box__container">
-              <div class="input-area">
-                <input id="img-file" type="file">
-                <label for="img-file"></label>
+        <form action="/post" method="post">
+          {{ csrf_field() }}
+          <!-- value仮入れ(Userモデルとリレーションするのに必要) -->
+          <input type="hidden" name="user_id" value="1">
+          <div class="sell__block__form__upload">
+            <h3 class="sell__block__form__upload__head">
+              スニーカの写真
+              <span class="require">必須</span>
+            </h3>
+            <div id="image-box-1">
+              <div class="item-num-0" id="image-box__container">
+                <div class="input-area">
+                  @if($errors->has('image'))
+                    <div class="error_msg">{{ $errors->first('image') }}</div>
+                  @endif
+                  <input id="img-file" type="file" name="image" value="{{ old('image') }}">
+                  <label for="img-file"></label>
+                </div>
               </div>
+            </div>
+            <div class="sell__block__form__name">
+              <div class="form-group__name select1">
+                <label>商品名<span class="require">必須</span></label>
+                <div>
+                  @if($errors->has('name'))
+                   <div class="error_msg">{{ $errors->first('name') }}</div>
+                  @endif
+                  <input class="form__group__name title" type="text" name="name" placeholder="商品名" value="{{ old('name') }}">
+                </div>
+                <label>ブランド<span class="require">必須</span></label>
+                <div>
+                  @if($errors->has('brand'))
+                    <div class="error_msg">{{ $errors->first('brand') }}</div>
+                  @endif
+                  <input class="form__group__name title" type="text" name="brand" placeholder="ブランド" value="{{ old('brand') }}">
+                </div>
+              </div>
+              <div class="sell__block__form__btn">
+                <div>
+                  <input class="btn-default__btn-red" type="submit" value="投  稿">
+                  <a href="/">戻る</a>
+                </div>
+              </div>
+            
             </div>
           </div>
-          <div class="sell__block__form__name">
-            <div class="form-group__name select1">
-              <label>商品名<span class="require">必須</span></label>
-              <div>
-                <input class="form__group__name title" type="text" placeholder="商品名">
-              </div>
-              <label>ブランド<span class="require">必須</span></label>
-              <div>
-                <input class="form__group__name title" type="text" placeholder="ブランド">
-              </div>
-            </div>
-            <div class="sell__block__form__btn">
-              <div>
-                <btn class="btn-default__btn-red" type="submit">投稿</btn>
-                <a href="/">戻る</a>
-              </div>
-            </div>
-
-          </div>
-        </div>
+        </form>  
       </div>
     </section>
   </main>
