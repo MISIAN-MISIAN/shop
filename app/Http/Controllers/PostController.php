0a5213333af69a031ab7ac63c18b2d81937cd048
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-03-03 23:49:01 +0900

    追記
    追記

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 // use Illuminate\Http\Support\Facades\View;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+
+use App\Post;
+use Validator;
+
 class PostController extends Controller
 {
     public function index(){
