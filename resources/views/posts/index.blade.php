25340225f5c77b78b9b30252e7d5d9df9271febf
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-03-06 22:02:56 +0900

    show
    show追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,7 +30,7 @@
               @foreach($posts as $post) 
                 <li class="image">
                   <div class="product">
-                      <a class="product-image" href="#">
+                      <a class="product-image" href="post/{id}">
                         <div class="product-card">
                           <span class="product-price">{{ $post->brand }}</span>
                           <img class="product-image-size" src="{{ $post->image }}" alt="商品写真">
