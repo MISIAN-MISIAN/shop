5e0edd3d1ea09755826d1360d77b99af07d7d5b0
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-03-09 23:47:19 +0900

    show
    contllor

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -60,7 +60,8 @@ class PostController extends Controller
     public function show($id){
         $post = Post::find($id);
         // return view('post/show',compact('post'));
-        return view('post/show',['post' => Post::findOrFail($id)]);
+        // return view('post/show',['post' => Post::findOrFail($id)]);
+        return view('post.show',['post' => $post]);
     }
 }
 
