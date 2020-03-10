09f496e91f7bd6fea6a5d829ebc72e1eebb3e566
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-03-10 23:26:17 +0900

    修正
    修正

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,45 +10,51 @@
 <body>
 @extends('layouts.app')
 @section('content')
-  <div class="item-detail">
-    <div class="item-detail-contents">
-      <h1 class="item-detail-name">アイテム名</h1>
-      <div class="item-detail-main clearfix">
-        <div class="item-detail-photo">
-          <img>
+  @if(count($post) > 0)
+    @foreach($post as $p)
+      <div class="item-detail">
+        <div class="item-detail-contents">
+          <h1 class="item-detail-name">アイテム名</h1>
+          <div class="item-detail-main clearfix">
+            <div class="item-detail-photo">
+              <img>
+            </div>
+            <table class="item-detail-table">
+              <tbody>
+                <tr>
+                  <th>投稿者</th>
+                  <td>
+                    <a class="item-detail-username">ユザー名</a>
+                    
+                  </td>
+                </tr>
+                <tr>
+                  <th>商品名</th> 
+                  <td>
+                    <a class="item-detail-category">{{ $p->name }}</a>
+                  </td>
+                </tr>
+                <tr>
+                  <th>ブランド</th>
+                  <td>
+                    {{ $p->brand }}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="item-detail-price-box">
+            <span class="item-detail-price">値段</span>
+            <span class="item-detail-tax">（税込）</span>
+          </div>
+
+
         </div>
-        <table class="item-detail-table">
-          <tbody>
-            <tr>
-              <th>投稿者</th>
-              <td>
-                <a class="item-detail-username">ユザー名</a>
-                
-              </td>
-            </tr>
-            <tr>
-              <th>商品名</th> 
-              <td>
-                <a class="item-detail-category"></a>
-              </td>
-            </tr>
-            <tr>
-              <th>ブランド</th>
-              <td>
-                ブランド名
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-      <div class="item-detail-price-box">
-        <span class="item-detail-price">値段</span>
-        <span class="item-detail-tax">（税込）</span>
-      </div>
-
-
-    </div>
-  </div>
+    @endforeach
+  @else
+    <div>投稿記事がありません</div>
+  @endif
 @endsection
 </body>
 </html>
