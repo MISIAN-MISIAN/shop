7fb00151b856dff972fd70c28ed11f416d85d78a
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-03-14 23:09:31 +0900

    edit
    routes追加

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('post/new', 'PostController@new');
 Route::post('post/create', 'PostController@create')->name('post.create'); // 保存
 Route::get('post/create', 'PostController@create')->name('post.create'); // 作成
 Route::get('post/show/{id}', 'PostController@show')->name('post.show');
+Routes::get('post/edit/{id}', 'PostController@edit');
 
 
 
