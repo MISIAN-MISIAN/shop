58f43f375e1ae5df781ebe6bd0ed03c151707712
MISIAN-MISIAN <baikinman0907kitanai@icloud.com>
2020-04-03 00:04:54 +0900

    修正
    修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,10 +12,10 @@
     <div class="nav">
       <h2 class="popularity-category">カテゴリー</h2>
       <div class="categories">
-        <a class="women button" href="#women">トップス</a>
-        <a class="men button" href="#men">パンツ</a>
-        <a class="appliance button" href="#appliance">シューズ</a>
-        <a class="toy button" href="#toy">その他(小物.アクセサリーetc)</a>
+        <a class="women button" href="#women">ナイキ</a>
+        <a class="men button" href="#men">アディダス</a>
+        <a class="appliance button" href="#appliance">ニューバランス</a>
+        <a class="toy button" href="#toy">その他</a>
       </div>
     </div>
     <div class="items-holders" >
